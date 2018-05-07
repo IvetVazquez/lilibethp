4fac1e83e0fd7e278c3187a8a29f7b05ebef5a4c
Brian <brian.hernandez@globant.com>
2018-05-07 15:52:25 -0500

    moved img

--- signup/alta.php
+++ signup/alta.php
@@ -1,2 +1,13 @@
 <?php
-  print_r($_POST);
+  include('./MailChimp.php');
+  use \DrewM\MailChimp\MailChimp;
+
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+
+  $MailChimp = new MailChimp('8054672f7391700d9f842e8ae8338979-us15');
+  $result = $MailChimp->get('lists');
+
+  print_r($result);
+  print_r($name);
+  print_r($email);
