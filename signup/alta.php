ae8815b7120b0708f5abe0e443a2868e7ad3b888
Brian <brian.hernandez@globant.com>
2018-05-07 16:05:53 -0500

    merge fields testing

--- signup/alta.php
+++ signup/alta.php
@@ -4,10 +4,12 @@
 
   $name = $_POST["name"];
   $email = $_POST["email"];
+  $list_id = "68dd8a46c4";
 
   $MailChimp = new MailChimp('8054672f7391700d9f842e8ae8338979-us15');
-  $result = $MailChimp->get('lists');
+  $result = $MailChimp->post("lists/$list_id/members", [
+        'email_address' => $email,
+        'status'        => 'subscribed',
+      ]);
 
   print_r($result);
-  print_r($name);
-  print_r($email);
