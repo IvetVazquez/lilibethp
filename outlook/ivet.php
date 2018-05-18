a2e1c3019214acbedcfb3ab49b7eecbeb708cf91
thinkMX <bdragonsb3@gmail.com>
2018-05-18 10:19:39 +0000

    final env vars test

--- outlook/ivet.php
+++ outlook/ivet.php
@@ -1,5 +1,11 @@
 <?php
-  print_r(getenv());
-  print_r(getenv("HOLA"));
-  print_r($_ENV);
+  require '../vendor/autoload.php';
+
+  $dotenv = new Dotenv\Dotenv(__DIR__);
+  $dotenv->load();
+
   print_r("que show?");
+  print_r(getenv('HOLA'));
+  print_r(getenv('CHIDO'));
+  print_r(getenv('RASMUS', 'MEXAS'));
+
